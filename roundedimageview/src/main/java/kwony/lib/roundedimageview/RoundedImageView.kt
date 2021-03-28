42c82eda8ba433928389c754ab7e897fb4449d54
kwony <kwony.cho@gmail.com>
2021-03-28 12:53:24 +0900

    initial commit

---
+++ roundedimageview/src/main/java/kwony/lib/roundedimageview/RoundedImageView.kt
@@ -0,0 +1,9 @@
+package kwony.lib.roundedimageview
+
+import android.content.Context
+import android.util.AttributeSet
+import com.quarkworks.roundedframelayout.RoundedFrameLayout
+
+class RoundedImageView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) {
+
+}
